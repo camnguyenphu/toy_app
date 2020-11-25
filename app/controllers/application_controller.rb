709aba54627538568663e4e35014ff1b3ab2b1f3
Cam Nguyen <cam.nguyenphu@vti.com.vn>
2020-11-25 09:52:20 +0700

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
     def hello
+        #users: id(int), name (string), email (string)
+        #microposts: id(int), content(string), user_id (string)
         render html:"Cầm Nguyễn"
     end
 end
---
+++ app/models/micropost.rb
@@ -0,0 +1,8 @@
+class Micropost < ApplicationRecord
+    
+    belongs_to :user
+
+    validates :content, length: { minimum: 5 },
+                        presence: true
+
+end
